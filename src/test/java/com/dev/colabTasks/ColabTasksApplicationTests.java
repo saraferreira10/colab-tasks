04413f38ef8d32f65776fa538749e2f285c56227
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-02-25 17:43:09 -0300

    project created

---
+++ src/test/java/com/dev/colabTasks/ColabTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.colabTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColabTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
